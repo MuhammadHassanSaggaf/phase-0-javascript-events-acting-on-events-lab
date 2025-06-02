bf444c6fa3212a832ddbea0606172dac820ce155
Muhammad Hassan Saggaf <muhammadsaggaf2004@gmail.com>
2025-06-02 18:05:34 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function moveDodgerLeft() {
+  const dodger = document.getElementById('dodger');
+  const leftString = dodger.style.left || '0px';
+  const left = parseInt(leftString.replace('px', ''), 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const dodger = document.getElementById('dodger');
+  const leftString = dodger.style.left || '0px';
+  const left = parseInt(leftString.replace('px', ''), 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
